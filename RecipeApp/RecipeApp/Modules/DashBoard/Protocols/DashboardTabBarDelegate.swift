87071c1e128293af65eee980a0ac842fb5c8e65a
Bhumik Poshiya <bhumik.poshiya@simformsolutions.com>
2026-07-21 01:37:34 +0530

    TE-T733: Add coordinator delegate protocols for inter-screen communication
    - Introduce AddCoordinatorDelegate and DashboardTabBarDelegate, and
    extend DashboardCoordinatorDelegate so tab/child coordinators can
    communicate navigation events back up to their parents.

---
+++ RecipeApp/RecipeApp/Modules/DashBoard/Protocols/DashboardTabBarDelegate.swift
@@ -0,0 +1,10 @@
+//
+//  DashboardTabBarDelegate.swift
+//  RecipeApp
+//
+//  Created by Bhumik Poshiya on 20/07/26.
+//
+
+protocol DashboardTabBarDelegate: AnyObject {
+    func dashboardTabBarDidTapAdd()
+}
