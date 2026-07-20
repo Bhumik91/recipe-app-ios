7803ce3617d075f9ba40f3f4f9eec33774e52435
Bhumik Poshiya <bhumik.poshiya@simformsolutions.com>
2026-07-20 11:01:40 +0530

    TE-T377: Relocate OnBoarding and add Auth Protocols
    - Reorganize OnBoarding module files to Modules/OnBoarding
    - Implement OnBoardingCoordinator workflow steps
    - Define Login and Signup view controller action delegates
    - Clean up old OnBoarding feature paths

---
+++ RecipeApp/RecipeApp/Modules/Auth/Protocols/LoginViewControllerDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  LoginViewControllerDelegate.swift
+//  RecipeApp
+//
+//  Created by Bhumik Poshiya on 10/07/26.
+//
+
+protocol LoginViewControllerDelegate: AnyObject {
+    func loginDidSucceed()
+    func loginDidTapSignUp()
+}
---
+++ RecipeApp/RecipeApp/Modules/OnBoarding/OnBoardingCoordinator.swift
@@ -0,0 +1,42 @@
+//
+//  OnBoardingCoordinator.swift
+//  RecipeApp
+//
+//  Created by Bhumik Poshiya on 10/07/26.
+//
+import UIKit
+
+final class OnBoardingCoordinator: Coordinator {
+    // MARK: - Properties
+    var childCoordinators: [any Coordinator] = []
+    var navigationController: UINavigationController
+    weak var parentDelegate: OnBoardingCoordinatorDelegate?
+    private let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    // MARK: - Initializer
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    // MARK: - Coordinator
+    func start() {
+        showOnBoardingViewController()
+    }
+}
+// MARK: - Navigation
+extension OnBoardingCoordinator {
+    private func showOnBoardingViewController() {
+        guard let onBoardingVC = storyboard.instantiateViewController(
+            withIdentifier: "OnBoardingViewController"
+        ) as? OnBoardingViewController else {
+            fatalError("OnBoardingViewController not found in Main.storyboard")
+        }
+        
+        onBoardingVC.coordinatorDelegate = self
+        navigationController.setViewControllers([onBoardingVC], animated: false)
+    }
+}
+// MARK: - OnBoardingViewDelegates
+extension OnBoardingCoordinator: OnBoardingViewDelegates {
+    func onBoardingDidTapStartCook() {
+        self.parentDelegate?.onBoardingFlowDidFinish(self)
+    }
+}
